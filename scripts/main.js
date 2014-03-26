692f68c68d8b88f7a48e318646f1494cf669a6b5
nattyc <natty.coleman@gmail.com>
2014-03-26 13:27:42 -0700

    initial commit. having trouble with dom stuff and event binding, will continue

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+$("h3").css("color","tomato");
+$(".copyright").replaceWith('<p class="copyright">©2014 Badass</p>');
+$("nav a").on('click', function(){
+alert("sweet");
+});
+// $("a").on('hover', function(){
+//    &( this ).css("background-color","red");
+//  });
+
