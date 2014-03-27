f4657bb9f2123cee794fac47cb4a784f4baf7f30
nattyc <natty.coleman@gmail.com>
2014-03-27 00:29:27 -0700

    playing with more stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,18 @@
 $("h3").css("color","tomato");
 $(".copyright").replaceWith('<p class="copyright">©2014 Badass</p>');
 $("nav a").on('click', function(){
-alert("sweet");
-});
-// $("a").on('hover', function(){
-//    &( this ).css("background-color","red");
-//  });
+  alert("sweet");
+  });
+$("figure").click(function(){
+  $("img").toggle(35000,"swing");
+  });
+
+$( "p, ul, h1" ).click(function() {
+  $( this ).toggleClass( "highlight" );
+  });
+
+$( ".banner" ).hover(function() {
+  $( this ).toggleClass( "highlight" );
+  });
+
 
